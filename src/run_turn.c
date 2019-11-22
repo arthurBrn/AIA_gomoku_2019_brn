46c4cbe0d14b6519f7823a6e773a747acf743238
Baron <arthur.baron@epitech.eu>
2019-11-22 17:19:20 +0100

    [ADD] [run_turn]

--- src/run_turn.c
+++ src/run_turn.c
@@ -47,22 +47,20 @@ int run_turn(t_gomoku *gomoku, char *str)
 
     if (gomoku->start == 0) {
         write(2, GAME_NOT_STARTED, strlen(GAME_NOT_STARTED));
-        return (84);
+        return (ERROR);
     }
     if (check_turn_errors(gomoku, str) != 0) {
         write(2, ERROR_MSG, strlen(ERROR_MSG));
-        return (84);
+        return (ERROR);
     }
     tab = malloc(sizeof(int) * (strlen(str)) + 4);
     init_begin(gomoku);
     new_str = malloc(sizeof(char) + 10);
     new_str = strcat(str, ",");
     new_str[index] = gomoku->player + 48;
-    puts("NEW_STR");
-    puts(new_str);
     if (store_board(new_str, gomoku) == 84) {
         write(2, ERROR_MSG, strlen(ERROR_MSG));
-        return (84);
+        return (ERROR);
     }
     free(new_str);
     free(tab);

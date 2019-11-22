febb09018042af41ff1e803b9c715d817d69919a
user <user@localhost.localdomain>
2019-11-22 13:42:35 +0100

    [modified] [run_board] exit added.

--- src/run_board.c
+++ src/run_board.c
@@ -12,8 +12,10 @@ int run_board(t_gomoku *gomoku, char *s1) {
     ssize_t size = 0;
     char *str = "";
 
-    if (gomoku->start == 0)
-        return (puts(GAME_NOT_STARTED));
+    if (gomoku->start == 0) {
+        write(2, GAME_NOT_STARTED, strlen(GAME_NOT_STARTED));
+        exit(ERROR);
+    }
     while (strcmp(str, "DONE") != 0) {
         if ((str = malloc(sizeof(char) * BUFF_SIZE)) == NULL)
             return (ERROR);

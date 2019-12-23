621dd51061cf85ba0e47546e4d28590371543bbd
Baron <arthur.baron@epitech.eu>
2019-12-23 13:36:50 +0100

    [DELETE][my_itoa][EDIT][utils]

--- src/utils.c
+++ src/utils.c
@@ -14,3 +14,12 @@ int isnum(char *str)
             return (MY_EXIT_FAILURE);
     return (0);
 }
+
+char *itoa(int nb, char *str, int size) {
+    str[size] = '\0';
+    while (size--) {
+        str[size] = (nb % 10) + '0';
+        nb = nb / 10;
+    }
+    return (str);
+}

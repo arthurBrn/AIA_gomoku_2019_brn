46bf516c5a511a2dd6ab63c50ff32464919de7b0
Baron <arthur.baron@epitech.eu>
2019-12-27 18:11:27 +0100

    [EDIT][run_begin] [MSG][storing begin move does not work due to player = 0]

--- src/run_begin.c
+++ src/run_begin.c
@@ -19,6 +19,26 @@ int error_begin(t_gomoku *gomoku)
     return (0);
 }
 
+int store_begin_move(t_gomoku *gomoku, char *xy)
+{
+    char *move_coordonate = malloc(sizeof(char) * 5);
+
+    move_coordonate[0] = *xy;
+    move_coordonate[1] = ',';
+    move_coordonate[2] = *xy;
+    move_coordonate[3] = ',';
+    move_coordonate[4] = (gomoku->player + '0');
+    move_coordonate[5] = '\0';
+    //printf("%s", move_coordonate);
+    printf("%d", gomoku->start);
+    if (storing_error(move_coordonate, gomoku, ",") == MY_EXIT_FAILURE)
+        return (MY_EXIT_FAILURE);
+    if (error_board(gomoku, "") == MY_EXIT_FAILURE)
+        return (MY_EXIT_FAILURE);
+    gomoku->board = store_move(gomoku->board, move_coordonate, ",");
+    return (0);
+}
+
 int run_begin(char *str, t_gomoku *gomoku)
 {
     (void) str;
@@ -31,12 +51,14 @@ int run_begin(char *str, t_gomoku *gomoku)
         gomoku->begin = 1;
         play = malloc(sizeof(char) * 4);
         play = itoa(gomoku->size / 2, play, 2);
-	new_play = delete_zero(play);
+	    new_play = delete_zero(play);
         write(1, new_play, strlen(new_play));
         write(1, ",", 1);
         write(1, new_play, strlen(new_play));
         write(1, "\n", 1);
         // remplir le tableau avec notre coup joué
+        if (store_begin_move(gomoku, new_play) == MY_EXIT_FAILURE)
+            return (MY_EXIT_FAILURE);
         return (0);
     }
 }
